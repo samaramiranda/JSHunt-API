16d4277f7c776d4a653b12fc483852d43bdf9e9c
samaramiranda <samaramiranda.13@hotmail.com>
2020-01-31 20:53:55 -0200

    Comentando código

--- src/controllers/ProductController.js
+++ src/controllers/ProductController.js
@@ -4,8 +4,8 @@ const Product = mongoose.model('Product'); //importando o model da aplicação
 
 
 //EXPORTANDO MÓDULO COM FUNÇOES
-///req significa a requisição que o user tá fazendo ao servidor. 
-//Posso pegar do "req" parametros, corpo da requisição, cabeçalho, usuario que ta fazendo, ip, autenticação...
+///req significa a requisição que o user está fazendo ao servidor. 
+//Posso pegar do "req" parametros, corpo da requisição, cabeçalho, usuario que está fazendo, ip, autenticação...
 //res significa a resposta que vamos dar pra requisição. Tem todas as informações para devolver uma resposta para o usuário
 module.exports = { 
   //LISTAGEM
@@ -13,20 +13,18 @@ module.exports = {
     const { page = 1 } = req.query; //usando destruturação. querry é usando para parametros get
     const products = await Product.paginate({}, { page, limit: 10 }); //procurando um product no DB
     //usando o paginate. 1 parametro é para where (filtro) e segundo parametro é a pagina atual e o tamanho
-    return res.json(products) //retornando em estrutura json
+    return res.json(products); //retornando em estrutura json
   },
 
   //DETALHE DO PRODUTO
   async show(req, res) {
     const product = await Product.findById(req.params.id); //busca pelo ID dentro dos parametros da rota
-
     return res.json(product);
   },
 
   //CRIAÇÃO
   async store(req, res) {
     const product = await Product.create(req.body); //estou fazendo uma requisição no body que é onde estão os dados do product
-
     return res.json(product); //retornando o produto que acabou de ser criado
   },
 
@@ -40,8 +38,7 @@ module.exports = {
 
   //DELETAR O PRODUTO
   async destroy(req, res) {
-    await Product.findByIdAndRemove(req.params.id); //nao preciso usar const product pq ele só vai deletar
-
-    return res.send(); //retorna uma msg de sucesso sem conteudo
+    await Product.findByIdAndRemove(req.params.id); //nao preciso usar const product porque ele só vai deletar
+    return res.send(); //retorna uma mensagem de sucesso sem conteudo
   },
 };
